4ea1c4fa1d837ee71388147640593ec5d204916d
karlygach <karygachkenenbaeva@gmail.com>
2021-04-02 15:20:25 +0600

    добавлена модель Category, изменено поле category у модели Product на ManyToManyField

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 
-from webapp.models import Product
+from webapp.models import Product, Category
 
 
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['id', 'name', 'category', 'remainder']
+    list_display = ['id', 'name', 'remainder']
     list_filter = ['category', 'price']
     search_fields = ['name']
     fields = ['id', 'name', 'category', 'remainder']
@@ -12,6 +12,6 @@ class ProductAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Product, ProductAdmin)
-
+admin.site.register(Category)
 
 # Register your models here.
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,15 +1,10 @@
 from django.db import models
 
-category_choices = [('other', 'Другое'),
-                    ('laptops', 'Ноутбуки'),
-                    ('tablets', 'Планшеты'),
-                    ('accessories', 'аксессуары')]
-
 
 class Product(models.Model):
     name = models.CharField(max_length=100, null=False, blank=False)
     description = models.TextField(max_length=2000, null=False, blank=True)
-    category = models.CharField(max_length=100, choices=category_choices, default='other')
+    category = models.ManyToManyField('webapp.Category', related_name='products', db_table='product_categories')
     remainder = models.PositiveIntegerField(null=False, blank=False)
     price = models.DecimalField(max_digits=7, decimal_places=2)
 
@@ -20,4 +15,17 @@ class Product(models.Model):
 
     def __str__(self):
         return f'{self.id}. {self.name}: {self.category}'
+
+
+class Category(models.Model):
+    category = models.CharField(max_length=150, default='other', verbose_name='Категория')
+
+    class Meta:
+        db_table = 'categories'
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return self.category
+
 # Create your models here.
