0edb3301c942a8bf85d5d2b1a1edd738bb544052
karlygach <karygachkenenbaeva@gmail.com>
2021-05-27 15:02:11 +0600

    подключен api в главный urls

--- source/hello/urls.py
+++ source/hello/urls.py
@@ -16,20 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from webapp.views import (
-    ProductListView,
-    ProductDetailView,
-    ProductCreateView,
-    ProductUpdateView,
-    ProductDeleteView,
-    ProductsInCartView,
-    AddProductView,
-    CartProductDelete,
-    # OrderView
-)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('webapp.urls')),
-    path('accounts/', include('accounts.urls'))
+    path('accounts/', include('accounts.urls')),
+    path('api/', include('api.urls'))
 ]
